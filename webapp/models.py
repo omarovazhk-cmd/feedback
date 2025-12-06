067e3fbe3206d37dfe581cbca3d66e4dc3cccab0
Zhanar <omarovazhk@gmail.com>
2025-12-06 12:18:45 +0500

    Создала модель товарвов/услуг и отзывов

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+CATEGORY_CHOICES = [
+    ('tech', 'Техника'),
+    ('food', 'Еда'),
+    ('beauty', 'Красота'),
+    ('services', 'Услуги'),
+    ('other', 'Другое'),
+]
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    text = models.TextField()
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1),MaxValueValidator(5)]
+    )
+    is_moderated = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Review by {self.author} for {self.product}"
